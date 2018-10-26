f99efe820a5239afe6bf587745ea4f20735c5e23
Alexander Farrow <AlexanderFarrow98@gmail.com>
2018-10-26 21:26:50 +0100

    Initial commit.

--- src/main/java/answers/Question1.java
+++ src/main/java/answers/Question1.java
@@ -3,8 +3,24 @@ package answers;
 public class Question1 {
 
 	public static int bestMergedPortfolio(int[] portfolios) {
-		// TODO Auto-generated method stub
-		return -1;
+
+        int maxIndex = (portfolios.length - 1);
+
+        int mergedPortfolio;
+        int bestMergedPortfolio = -1;
+
+        for (int i = 0; i <= maxIndex-1; i++) {
+
+            for (int j = i+1; j <= maxIndex; j++) {
+                mergedPortfolio = portfolios[i] ^ portfolios[j];
+
+                if (mergedPortfolio > bestMergedPortfolio) {
+                    bestMergedPortfolio = mergedPortfolio;
+                }
+            }
+        }
+
+		return bestMergedPortfolio;
 	}
 
 }
