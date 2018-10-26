49827687290b1aaf50d413bd76032ac2ecda69e2
Alexander Farrow <AlexanderFarrow98@gmail.com>
2018-10-26 21:51:34 +0100

    Replace my code with the original code to see if that removes the error.

--- src/main/java/answers/Question1.java
+++ src/main/java/answers/Question1.java
@@ -3,24 +3,8 @@ package answers;
 public class Question1 {
 
 	public static int bestMergedPortfolio(int[] portfolios) {
-
-        int maxIndex = (portfolios.length - 1);
-
-        int mergedPortfolio;
-        int bestMergedPortfolio = -1;
-
-        for (int i = 0; i <= maxIndex-1; i++) {
-
-            for (int j = i+1; j <= maxIndex; j++) {
-                mergedPortfolio = portfolios[i] ^ portfolios[j];
-
-                if (mergedPortfolio > bestMergedPortfolio) {
-                    bestMergedPortfolio = mergedPortfolio;
-                }
-            }
-        }
-
-		return bestMergedPortfolio;
+		// TODO Auto-generated method stub
+		return -1;
 	}
 
 }
